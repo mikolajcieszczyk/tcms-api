0c9e7a289798a3c54dfc26a4930b148f32c0bd2e
Mikołaj Cięszczyk <39798058+mikolajcieszczyk@users.noreply.github.com>
2022-09-19 09:46:45 +0200

    added env setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ClientsModule } from './clients/clients.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ClientsModule, ClientsModule],
+  imports: [ConfigModule.forRoot(), ClientsModule, ClientsModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  console.log(process.env.PORT);
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
