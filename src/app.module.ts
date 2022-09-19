29421e568fce41329d5cf2607377bc4f764ae168
Mikołaj Cięszczyk <39798058+mikolajcieszczyk@users.noreply.github.com>
2022-09-19 09:27:45 +0200

    created client module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientModule } from './client/client.module';
+import { ClientsModule } from './clients/clients.module';
 
 @Module({
-  imports: [],
+  imports: [ClientModule, ClientsModule],
   controllers: [AppController],
   providers: [AppService],
 })
